fd2a07ff0e083c45e5e211f25ec4301513aa8b5e
minsik0 <qwew1212@naver.com>
2025-12-05 21:14:28 +0900

    add: S3Config, S3Properties, S3Uploader, S3UploaderImpl 추가

---
+++ src/main/java/com/common/utils/s3/S3Uploader.java
@@ -0,0 +1,10 @@
+package com.common.utils.s3;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface S3Uploader {
+
+    String upload(MultipartFile file, String dirName);
+
+    void delete(String fileUrl);
+}
