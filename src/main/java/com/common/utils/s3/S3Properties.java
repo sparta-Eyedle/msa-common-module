3985e583d3fbaa452de5cdc0a4320547bd394fa4
minsik0 <qwew1212@naver.com>
2025-12-06 22:57:14 +0900

    add: S3Config, S3Properties, S3Uploader, S3UploaderImpl 추가

---
+++ src/main/java/com/common/utils/s3/S3Properties.java
@@ -0,0 +1,22 @@
+package com.common.utils.s3;
+
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+@ConfigurationProperties(prefix = "cloud.aws")
+public class S3Properties {
+    private String accessKey;
+    private String secretKey;
+    private String region;
+    private String bucket;
+    private String baseUrl;
+
+    public void setAccessKey(String accessKey) { this.accessKey = accessKey; }
+    public void setSecretKey(String secretKey) { this.secretKey = secretKey; }
+    public void setRegion(String region) { this.region = region; }
+    public void setBucket(String bucket) { this.bucket = bucket; }
+    public void setBaseUrl(String baseUrl) { this.baseUrl = baseUrl; }
+}
